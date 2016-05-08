3dad51976693ea9d2c502554df0ec25d05220f07
flyspeck <flyspeck@mail.com>
2016-05-08 18:27:57 -0400

    updated pent.m for completeness

--- cryptography/group_law_edwards/edwards.m
+++ cryptography/group_law_edwards/edwards.m
@@ -10,7 +10,6 @@ delta23 = delta[x2,y2,x3,y3];
 deltad[d_, x1_, y1_, x2_, y2_] := 1 + d x1 x2 y1 y2;
 
 delta12 - deltad[d, x1, y1, x2, y2] deltad[-d, x1, y1, x2, y2] // Simplify
-  
 
 plus[{x1_, y1_}, {x2_, y2_}] :=
   {
@@ -33,7 +32,6 @@ deltad[d,x1, y1, x1p, y1p] // Factor;
 
 deltaY = deltaX/.{d->(-d)};
 
-
 {gx, gy} = (plus[{x3p,y3p}, {x3, y3}] - 
        plus[{x1, y1}, {x1p,y1p}]) // Together // Factor;
        
@@ -45,16 +43,21 @@ polyassoc = PolynomialReduce[{gxpoly, gypoly}, {e1,e2,e3},
 
 (* completeness identity *)
 
+complete = 
+{d^2 x1^2 x2^2 y2^2 e1 +  (1 - d x1^2) delta[x1, y1, x2, y2] - 
+  d x1^2 e2 , (1 - c d x1^2 x2^2) (1 - d x1^2 y2^2) };
+
+completereduce = complete//Expand;
+
+(* Bernstein-Lange version of completeness
 complete = {
 d x1^2 y1^2 (t x2 + y2)^2 - e2 d x1^2 y1^2 + 
       e1 + (1 - y1^2) delta12 - (t x1 + d x1 y1 x2 y2 y1)^2 +
     (c - t^2) x1^2 (-1 + d x2^2 y1^2),
-      
-     d x1^2 y1^2 (t x2 - y2)^2 - e2 d x1^2 y1^2 + 
+           d x1^2 y1^2 (t x2 - y2)^2 - e2 d x1^2 y1^2 + 
       e1 + (1 - y1^2) delta12 - (t x1 - d x1 y1 x2 y2 y1)^2 +
            (c - t^2) x1^2 (-1 + d x2^2 y1^2)};
-
-completereduce = complete//Expand;
+*)
      
 (* convert to HOL Light *)
 
@@ -73,26 +76,28 @@ ToHOL[x_Symbol] := ts[x];
 
 (* HOL Light material *)
 (* completeness *)
-ToHOL[complete[[1]]];
-ToHOL[complete[[2]]];
-(* closure *)
-ToHOL[groupclosure];
-ToHOL[e1];
-ToHOL[e2];
-ToHOL[polyclosure[[1,1]]];
-ToHOL[polyclosure[[1,2]]];
-(* associativity *)
-ToHOL[gxpoly];
-ToHOL[gypoly];
-ToHOL[e1];
-ToHOL[e2];
-ToHOL[e3];
-ToHOL[polyassoc[[1,1,1]]];
-ToHOL[polyassoc[[1,1,2]]];
-ToHOL[polyassoc[[1,1,3]]];
-ToHOL[polyassoc[[2,1,1]]];
-ToHOL[polyassoc[[2,1,2]]];
-ToHOL[polyassoc[[2,1,3]]];
+join[ToHOL[complete[[1]]]," = ",ToHOL[complete[[2]]]];
 
-(* Now ready to move to HOL-Light verification.
-   Time how long it takes from here *)
+(* closure *)
+join[ToHOL[groupclosure]," = ",
+     ToHOL[e1]," * ",
+     ToHOL[polyclosure[[1,1]]]," + ",
+     ToHOL[e2]," * ",
+     ToHOL[polyclosure[[1,2]]]];
+
+(* associativity *)
+join[ToHOL[gxpoly], " = ",
+     ToHOL[e1]," * ",
+     ToHOL[polyassoc[[1,1,1]]], " + ",
+     ToHOL[e2]," * ",
+     ToHOL[polyassoc[[1,1,2]]], " + ",
+     ToHOL[e3], "  * ",
+     ToHOL[polyassoc[[1,1,3]]]];
+
+join[ToHOL[gypoly], " = ",
+     ToHOL[e1]," * ",
+     ToHOL[polyassoc[[2,1,1]]], " + ",
+     ToHOL[e2]," * ",
+     ToHOL[polyassoc[[2,1,2]]], " + ",
+     ToHOL[e3], "  * ",
+     ToHOL[polyassoc[[2,1,3]]]];
